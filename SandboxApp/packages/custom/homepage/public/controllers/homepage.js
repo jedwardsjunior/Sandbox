12073d61c85b972cdca0b156ffce8db6ae1e4b2c
Julia Edwards <jedwardsjunior@gmail.com>
2014-10-13 20:54:25 +0100

    Basic search bar

---
+++ SandboxApp/packages/custom/homepage/public/controllers/homepage.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('mean.homepage').controller('HomepageController', ['$scope', 'Global', 'Homepage',
+  function($scope, Global, Homepage) {
+    $scope.global = Global;
+    $scope.query = "";
+    $scope.package = {
+      name: 'homepage'
+    };
+    $scope.submitQuery = function() {
+	if ($scope.query) {
+	    console.log($scope.query)
+	}
+    };
+  }
+]);
---
+++ SandboxApp/packages/custom/homepage/public/services/homepage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('mean.homepage').factory('Homepage', [
+  function() {
+    return {
+      name: 'homepage'
+    };
+  }
+]);
